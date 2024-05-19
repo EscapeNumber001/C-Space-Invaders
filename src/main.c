b5fcc592f73c065c945a7f9f3ad388d57c7041a4
EscapeNumber001 <19634710+EscapeNumber001@users.noreply.github.com>
2024-05-19 17:20:51 -0400

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/19/2024 04:52:30 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+int main()
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+  return 0;
+}
