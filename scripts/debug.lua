c3e3b8783fc6611d94c491f201a2b125e1347f7d
EscapeNumber001 <19634710+EscapeNumber001@users.noreply.github.com>
2024-07-14 19:47:42 -0400

    Lua should now be completely removed

--- scripts/debug.lua
+++
@@ -1,8 +0,0 @@
-x = CreateEntity()
-LoadTexture('assets/animationtest.bmp', 64, 15, true)
-SetEntityTexture(x, 'assets/animationtest.bmp')
-print("Hello world")
-y = GetEntities()
-print(#y)
-MoveEntity(y[1], 100, 100)
-DestroyEntity(y[2])
